48af2744cb5debf5b2181aa68a5c16997403ffdc
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-26 22:17:22 +0200

    Created a basic form UI to test POST API action

--- src/client-app/src/App.js
+++ src/client-app/src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      employees: [],
+      name: '',
+      email: ''
+    }
+    this.nameInputChangeHandler = this.nameInputChangeHandler.bind(this);
+    this.emailInputChangeHandler = this.emailInputChangeHandler.bind(this);
+  }
+
+  nameInputChangeHandler(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  emailInputChangeHandler(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  formSubmitHandler(event) {
+    event.preventDefault();
+    fetch('http://localhost:5000/api/employees', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content': 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email
+      })
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Thundrware's Employees Directory</h1>
+        <form>
+          <input type="text" placeholder="Input name" onChange={this.nameInputChangeHandler} value={this.state.name}/><br/>
+          <input type="text" placeholder="Input email" onChange={this.emailInputChangeHandler} value={this.state.email}/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
+        
       </div>
     );
   }
